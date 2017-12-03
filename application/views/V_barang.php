41d28449b7d8146e75b2deed409706aba116510d
Muhammad Adam Fahmil 'Ilmi <adamfahmil020@gmail.com>
2017-12-03 21:52:17 +0700

    Penyesuaian Laman Barang (baru 2)
    Ada laman barang list (menggunakan data table) dan barang create

--- application/views/V_barang.php
+++ application/views/V_barang.php
@@ -5,120 +5,100 @@
   <?php include('template/V_header.php'); ?>
 </head>
 
-<body>
-
+<body class="hold-transition skin-blue sidebar-mini">
     <div id="wrapper">
-
         <!-- Navigation -->
         <?php include('template/V_navbar.php'); ?>
-
         <!-- Page Content -->
-        <div id="page-wrapper">
-          <div class="row">
-              <div class="col-lg-12">
-                  <h1 class="page-header">Barang</h1>
-              </div>
-              <!-- /.col-lg-12 -->
-          </div>
-          <!-- /.row -->
-          <div class="row">
-              <div class="col-lg-12">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                      Tabel Barang
-                  </div>
-                  <div class="panel-body">
-                    <div class="row" style="margin-bottom: 10px">
-                      <div class="col-md-4">
-                        <?php echo anchor(site_url('C_barang/create'),'Tambah', 'class="btn btn-primary"'); ?>
-                      </div>
-                      <div class="col-md-4 text-center">
-                        <div style="margin-top: 8px" id="message">
-                          <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
-                        </div>
-                      </div>
-                      <div class="col-md-1 text-right"></div>
-                      <div class="col-md-3 text-right">
-                        <form action="<?php echo site_url('C_barang/index'); ?>" class="form-inline" method="get">
-                          <div class="input-group">
-                            <input type="text" class="form-control" name="q" value="<?php echo $q; ?>">
-                            <span class="input-group-btn">
-                              <?php
-                                  if ($q <> '')
-                                  {
-                                    ?>
-                                    <a href="<?php echo site_url('C_barang'); ?>" class="btn btn-default">Reset</a>
-                                    <?php
-                                  }
-                              ?>
-                              <button class="btn btn-primary" type="submit">Cari</button>
-                            </span>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                    <table class="table table-bordered" style="margin-bottom: 10px">
-                      <tr>
-                        <th>No</th>
-                    		<th>ID Barang</th>
-                    		<th>Nama Barang</th>
-                        <th>Jumlah Barang</th>
-                    		<th>Harga Barang</th>
-                    		<th>Menu</th>
-                      </tr><?php
-                      foreach ($c_barang_hrv_data as $c_barang_hrv)
-                      {
-                        ?>
-                        <tr>
-                    			<td width="80px"><?php echo ++$start ?></td>
-                          <td><?php echo $c_barang_hrv->id_jenis ?><?php echo $c_barang_hrv->id_merk ?></td>
-                    			<td><?php echo $c_barang_hrv->nama_barang ?></td>
-                    			<td><?php echo $c_barang_hrv->jumlah_barang ?></td>
-                    			<td><?php echo $c_barang_hrv->harga_barang ?></td>
-                    			<td style="text-align:center" width="200px">
-                    				<?php
-                    				echo anchor(site_url('C_barang/update/'.$c_barang_hrv->id),'<i class="glyphicon glyphicon-edit" data-toggle="tooltip" title="Ubah"></i>');
-                    				echo "\t\t | \t\t";
-                    				echo anchor(site_url('C_barang/delete/'.$c_barang_hrv->id),'<i class="glyphicon glyphicon-trash" data-toggle="tooltip" title="Hapus"></i>','onclick="javasciprt: return confirm(\'Apakah Anda yakin ?\')"');
-                    				?>
-                    			</td>
-                    		</tr>
-                          <?php
-                      }
+        <div class="content-wrapper">
+          <section class="content-header">
+            <div class="col-lg-12">
+              <h1 class="page-header">Barang</h1>
+            </div>
+          </section>
+          <section class="content">
+            <div class="col-lg-12">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Tabel Barang</h3>
+                  <?php echo anchor(site_url('C_barang/excel'), 'Expor ke Excel', 'class="btn btn-primary btn-info"'); ?>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                      <th>No</th>
+                  		<th>ID Barang</th>
+                  		<th>Nama Barang</th>
+                      <th>Jumlah Barang</th>
+                  		<th>Harga Barang</th>
+                  		<th>Menu</th>
+                    </tr>
+                    </thead>
+                    <tbody><?php
+                    foreach ($c_barang_hrv_data as $c_barang_hrv)
+                    {
                       ?>
-                    </table>
-                    <div class="row">
-                      <div class="col-md-6">
-                        <a class="btn btn-primary">Total : <?php echo $total_rows ?></a>
-                    		<?php echo anchor(site_url('C_barang/excel'), 'Expor ke Excel', 'class="btn btn-primary"'); ?>
-                	    </div>
-                      <div class="col-md-6 text-right">
-                          <?php echo $pagination ?>
-                      </div>
-                    </div>
-                  </div>
+                      <tr>
+                  			<td><?php echo ++$start ?></td>
+                        <td><?php echo $c_barang_hrv->id_jenis ?><?php echo $c_barang_hrv->id_merk ?></td>
+                  			<td><?php echo $c_barang_hrv->nama_barang ?></td>
+                  			<td><?php echo $c_barang_hrv->jumlah_barang ?></td>
+                  			<td>Rp <?php echo $c_barang_hrv->harga_barang ?></td>
+                  			<td style="text-align:center">
+                  				<?php
+                  				echo anchor(site_url('C_barang/update/'.$c_barang_hrv->id),'<i class="glyphicon glyphicon-edit" data-toggle="tooltip" title="Ubah"></i>');
+                  				echo "\t\t | \t\t";
+                  				echo anchor(site_url('C_barang/delete/'.$c_barang_hrv->id),'<i class="glyphicon glyphicon-trash" data-toggle="tooltip" title="Hapus"></i>','onclick="javasciprt: return confirm(\'Apakah Anda yakin ?\')"');
+                  				?>
+                  			</td>
+                  		</tr>
+                        <?php
+                    }
+                    ?>
+                    </tbody>
+                  </table>
                 </div>
               </div>
-              <!-- /.col-lg-12 -->
-          </div>
-          <!-- /.row -->
+            </div>
+            <!-- /.col-lg-12 -->
+          </section>
         </div>
         <!-- /#page-wrapper -->
 
     </div>
     <!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/dist/js/sb-admin-2.js"></script>
+    <!-- jQuery 3 -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- DataTables -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <!-- SlimScroll -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
+    <!-- page script -->
+    <script>
+      $(function () {
+        $('#example1').DataTable()
+        $('#example2').DataTable({
+          'paging'      : true,
+          'lengthChange': false,
+          'searching'   : false,
+          'ordering'    : true,
+          'info'        : true,
+          'autoWidth'   : false
+        })
+      })
+    </script>
 
 </body>
 
--- application/views/V_barang_create.php
+++ application/views/V_barang_create.php
@@ -5,76 +5,83 @@
   <?php include('template/V_header.php'); ?>
 </head>
 
-<body>
-
+<body class="hold-transition skin-blue sidebar-mini">
     <div id="wrapper">
-
         <!-- Navigation -->
         <?php include('template/V_navbar.php'); ?>
-
         <!-- Page Content -->
-        <div id="page-wrapper">
-          <div class="row">
-              <div class="col-lg-12">
-                  <h1 class="page-header">Barang</h1>
-              </div>
-              <!-- /.col-lg-12 -->
-          </div>
-          <!-- /.row -->
-          <div class="row">
-              <div class="col-lg-12">
-                  <div class="panel panel-default">
-                      <div class="panel-heading">
-                          Tambah Barang
+        <div class="content-wrapper">
+          <section class="content-header">
+            <div class="col-lg-12">
+              <h1 class="page-header">Barang</h1>
+            </div>
+          </section>
+          <!-- /#content-header -->
+          <section class="content">
+            <div class="col-lg-12">
+              <div class="box box-info">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Tambah Barang</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form class="form-horizontal" action="<?php echo $action; ?>" method="post">
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label for="varchar" class="col-sm-2 control-label">Jenis <?php echo form_error('id_jenis') ?></label>
+                      <div class="col-sm-10">
+                        <select name="id_jenis" class="form-control">
+                          <?php foreach ($data_jenis_hrv as $data_jenis)
+                           { ?>
+                            <option value="<?php echo $data_jenis->id_jenis ?>"><?php echo $data_jenis->jenis ?></option>
+                          <?php } ?>
+                        </select>
                       </div>
-                      <!-- /.panel-heading -->
-                      <div class="panel-body">
-                        <form action="<?php echo $action; ?>" method="post">
-                    	    <div class="form-group">
-                                <label for="varchar">Jenis <?php echo form_error('id_jenis') ?></label>
-                                <select name="id_jenis" class="form-control">
-                                  <?php foreach ($data_jenis_hrv as $data_jenis)
-                                   { ?>
-                                    <option value="<?php echo $data_jenis->id_jenis ?>"><?php echo $data_jenis->jenis ?></option>
-                                  <?php } ?>
-                                </select>
-                            </div>
-                    	    <div class="form-group">
-                                <label for="varchar">Merk <?php echo form_error('id_merk') ?></label>
-                                <select name="id_merk" class="form-control">
-                                  <?php foreach ($data_merk_hrv as $data_merk)
-                                   { ?>
-                                    <option value="<?php echo $data_merk->id_merk ?>"><?php echo $data_merk->merk ?></option>
-                                  <?php } ?>
-                                </select>
-                            </div>
-                    	    <div class="form-group">
-                                <label for="varchar">Nama <?php echo form_error('nama_barang') ?></label>
-                                <input type="text" class="form-control" name="nama_barang" id="nama_barang" placeholder="Nama Barang" value="<?php echo $nama_barang; ?>" />
-                            </div>
-                    	    <div class="form-group">
-                                <label for="bigint">Jumlah <?php echo form_error('jumlah_barang') ?></label>
-                                <input type="number" min="0" class="form-control" name="jumlah_barang" id="jumlah_barang" placeholder="Jumlah Barang" value="<?php echo $jumlah_barang; ?>" />
-                            </div>
-                    	    <div class="form-group">
-                                <label for="int">Harga per Satuan <?php echo form_error('harga_barang') ?></label>
-                                <input type="number" min="1" class="form-control" name="harga_barang" id="harga_barang" placeholder="Harga Barang" value="<?php echo $harga_barang; ?>" />
-                            </div>
-                          <input type="hidden" name="waktu_tambah" id="waktu_tambah" value="<?php date_default_timezone_set("Asia/Jakarta"); echo date('Y-m-d H:i:s'); ?>" />
-                          <input type="hidden" name="waktu_update" id="waktu_update" value="0000-00-00 00:00:00" />
-                          <input type="hidden" name="flag_barang" id="flag_barang" value="0" />
-                    	    <input type="hidden" name="id" value="<?php echo $id; ?>" />
-                    	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button>
-                    	    <a href="<?php echo site_url('C_barang') ?>" class="btn btn-default">Cancel</a>
-                      	</form>
+                    </div>
+                    <div class="form-group">
+                      <label for="varchar" class="col-sm-2 control-label">Merk <?php echo form_error('id_merk') ?></label>
+                      <div class="col-sm-10">
+                        <select name="id_merk" class="form-control">
+                          <?php foreach ($data_merk_hrv as $data_merk)
+                           { ?>
+                            <option value="<?php echo $data_merk->id_merk ?>"><?php echo $data_merk->merk ?></option>
+                          <?php } ?>
+                        </select>
                       </div>
-                      <!-- /.panel-body -->
+                    </div>
+                    <div class="form-group">
+                      <label for="varchar" class="col-sm-2 control-label">Nama <?php echo form_error('nama_barang') ?></label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" name="nama_barang" id="nama_barang" placeholder="Tuliskan nama barang" value="<?php echo $nama_barang; ?>" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="bigint" class="col-sm-2 control-label">Jumlah <?php echo form_error('jumlah_barang') ?></label>
+                      <div class="col-sm-10">
+                        <input type="number" min="0" class="form-control" name="jumlah_barang" id="jumlah_barang" placeholder="Masukkan jumlah barang" value="<?php echo $jumlah_barang; ?>" />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="int" class="col-sm-2 control-label">Harga per Satuan (Rp)<?php echo form_error('harga_barang') ?></label>
+                      <div class="col-sm-10">
+                        <input type="number" min="1" class="form-control" name="harga_barang" id="harga_barang" placeholder="Harga Barang" value="<?php echo $harga_barang; ?>" />
+                      </div>
+                    </div>
+                      <input type="hidden" name="waktu_tambah" id="waktu_tambah" value="<?php date_default_timezone_set("Asia/Jakarta"); echo date('Y-m-d H:i:s'); ?>" />
+                      <input type="hidden" name="waktu_update" id="waktu_update" value="0000-00-00 00:00:00" />
+                      <input type="hidden" name="flag_barang" id="flag_barang" value="0" />
+                      <input type="hidden" name="id" value="<?php echo $id; ?>" />
                   </div>
-                  <!-- /.panel -->
+                  <!-- /.box-body -->
+                  <div class="box-footer">
+                    <a href="<?php echo site_url('C_barang') ?>" class="btn btn-default">Cancel</a>
+                    <button type="submit" class="btn btn-primary pull-right"><?php echo $button ?></button>
+                  </div>
+                  <!-- /.box-footer -->
+                </form>
               </div>
-              <!-- /.col-lg-12 -->
-          </div>
-          <!-- /.row -->
+            </div>
+          </section>
         </div>
         <!-- /#page-wrapper -->
 
@@ -83,33 +90,17 @@
 
     <input id="getBaseURL" type="hidden" class="default-hidden" value="<?php echo base_url();?>" />
 
-    <!-- jQuery -->
-    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+    <!-- jQuery 3 -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- DataTables JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/vendor/datatables-responsive/dataTables.responsive.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/dist/js/sb-admin-2.js"></script>
-
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true
-        });
-    });
-    </script>
-
-    <!-- <script src='<?php //echo base_url();?>assets/js/js_jenis.js'></script> -->
 </body>
 
 </html>
